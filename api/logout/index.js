fbb437655094197fc7111ccfd2d520121049f85c
Wizette <wizette2009@gmail.com>
2026-03-18 12:50:19 +0200

    Simplify auth: shared password gate, remove API/login/admin

--- api/logout/index.js
+++
@@ -1,9 +0,0 @@
-const { clearAuthCookie } = require('../shared/auth');
-
-module.exports = async function (context, req) {
-  context.res = {
-    status: 200,
-    headers: { 'Set-Cookie': clearAuthCookie() },
-    body: { message: 'Logged out.' }
-  };
-};
